d0e767af36c285789634331d84106903d10e8c38
Julian Leon <juliandgleon@gmail.com>
2021-09-01 12:02:06 -0500

    adding the rest of logic (methods) to the game class

--- ss.js
+++ ss.js
@@ -12,13 +12,82 @@ class Game {
 
     constructor() {
         this.initialize();
+        this.generateSequence();
+        this.nextLevel();
+
+        this.colors = {
+            blue: blue,
+            violet: violet,
+            orange: orange,
+            green: green,
+        };
     }
 
 
     // hide the "start" button
     initialize() {
         btnStart.classList.add( 'hide' );
-        this.level = 7;
+        this.level = 1;
+    }
+
+
+    // Create the sequence
+    generateSequence() {
+        this.sequence = new Array( 10 ).fill( 0 ).map( ( i ) => {
+            return( Math.floor( Math.random() * 4 ) );
+        });
+    }
+
+
+    // It shows the sequence by levels
+    nextLevel() {
+        this.showSequence();
+    }
+
+
+    // This functions is in charge of transforming numbers to colors
+    transformNTC( number ) {
+        switch( number ) {
+            case 0:
+                return "blue";
+            break;
+
+            case 1:
+                return "violet";
+            break;
+
+            case 2:
+                return "orange";
+            break;
+
+            case 3:
+                return "green";
+            break;
+        }
+    }
+
+
+    // Turn on (Or show) the sequence color to color
+    showSequence() {
+        for ( let i=0; i < this.level; i++ ) {
+            let color = this.transformNTC( this.sequence[i] );
+
+            setTimeout( () => { this.illuminateColor( color ) }, ( 1000 * i ) );
+        }
+    }
+
+
+    // Turn on each color of the sequence
+    illuminateColor( color ) {
+        this.colors[ color ].classList.add( 'light' );
+
+        setTimeout( () => { this.shutdownColor( color ) }, 350);
+    }
+
+
+    // Shutdown each color of the sequence
+    shutdownColor( color ) {
+        this.colors[ color ].classList.remove( 'light' );
     }
 
 }
